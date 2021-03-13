30db36cc0d9b2f0691264ea94a15ff8bda31d0ad
Ahsanul Haque <ahsanulalig@gmail.com>
2021-03-14 01:41:37 +0530

    icons issue resolved after deployment icons not shoing

--- projects/mtn-lib/src/lib/mtn-carousel/mtn-carousel.component.ts
+++ projects/mtn-lib/src/lib/mtn-carousel/mtn-carousel.component.ts
@@ -11,46 +11,46 @@ import { fromEvent, Observable, Subscription } from 'rxjs';
 export class MtnCarouselComponent implements OnInit {
   @Input() cards: Card[] = [
     {
-      imgPath: '../../assets/images/test.png',
+      imgPath: 'assets/images/test.png',
       title: 'Mobile internet',
       btnText: 'Start Here',
-      altText: 'Please add Path with prefix to root folder ../../',
+      altText: 'Please add Path with prefix to root folder',
     },
     {
-      imgPath: '../../assets/images/test.png',
+      imgPath: 'assets/images/test.png',
       title: 'Home internet',
       btnText: 'Start Here',
-      altText: 'Please add Path with prefix to root folder ../../',
+      altText: 'Please add Path with prefix to root folder ',
     },
     {
-      imgPath: '../../assets/images/test.png',
+      imgPath: 'assets/images/test.png',
       title: 'Get a device',
       btnText: 'Start Here',
-      altText: 'Please add Path with prefix to root folder ../../',
+      altText: 'Please add Path with prefix to root folder',
     },
     {
-      imgPath: '../../assets/images/test.png',
+      imgPath: 'assets/images/test.png',
       title: 'Add a phone line',
       btnText: 'Start Here',
-      altText: 'Please add Path with prefix to root folder ../../',
+      altText: 'Please add Path with prefix to root folder',
     },
     {
-      imgPath: '../../assets/images/test.png',
+      imgPath: 'assets/images/test.png',
       title: 'Upgrade',
       btnText: 'Start Here',
-      altText: 'Please add Path with prefix to root folder ../../',
+      altText: 'Please add Path with prefix to root folder',
     },
     {
-      imgPath: '../../assets/images/test.png',
+      imgPath: 'assets/images/test.png',
       title: 'Fixed the issue',
       btnText: 'Start Here',
-      altText: 'Please add Path with prefix to root folder ../../',
+      altText: 'Please add Path with prefix to root folder',
     },
     {
-      imgPath: '../../assets/images/test.png',
+      imgPath: 'assets/images/test.png',
       title: 'Customer Care',
       btnText: 'Start Here',
-      altText: 'Please add Path with prefix to root folder ../../',
+      altText: 'Please add Path with prefix to root folder',
     },
   ];
 
